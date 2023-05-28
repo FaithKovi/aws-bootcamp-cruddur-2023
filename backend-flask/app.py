701269aca5a7d3f33db8b843375d19c7f4d5ace0
FaithKovi <faithkovivera@gmail.com>
2023-05-28 14:46:15 +0000

    W8: Fix handle errors

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -178,10 +178,13 @@ def data_messages(message_group_uuid):
 @app.route("/api/messages", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_create_message():
-  message_group_uuid   = request.json.get('message_group_uuid',None)
-  user_receiver_handle = request.json.get('handle',None)
-  message = request.json['message']
-  access_token = extract_access_token(request.headers)
+  model = CreateMessage.run(
+    mode="update",
+    message=message,
+    message_group_uuid=message_group_uuid,
+    cognito_user_id=cognito_user_id,
+    user_receiver_handle=user_receiver_handle
+  )
   try:
     claims = cognito_jwt_token.verify(access_token)
     # authenicatied request
